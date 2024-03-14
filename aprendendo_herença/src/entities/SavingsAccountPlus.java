afc91c96645d359aea9c4ef2f3a45beb7f4d96b8
Pedro Henrique de Souza e Silva <desouzaesilvap@gmail.com>
2024-03-14 14:54:03 -0300

    Atualizando o save

--- aprendendo_herença/src/entities/SavingsAccountPlus.java
+++ aprendendo_herença/src/entities/SavingsAccountPlus.java
@@ -1,5 +1,9 @@
 package entities;
 
-public class SavingsAccountPlus {
-
+public class SavingsAccountPlus extends SavingsAccount{
+	
+	@Override
+	public void withdraw(double amount) {
+		balance -= amount + 2.0;
+	}
 }
