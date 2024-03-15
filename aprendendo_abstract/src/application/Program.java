a7c32dfd44e4d3706dbdcfb86532fa9b421720e0
Pedro Henrique de Souza e Silva <desouzaesilvap@gmail.com>
2024-03-15 19:18:35 -0300

    Adicionando exercicio de abstração resolvido por mim

---
+++ aprendendo_abstract/src/application/Program.java
@@ -0,0 +1,53 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Circle;
+import entities.Rectangle;
+import entities.Shape;
+import entities.enums.Color;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the number of shapes: ");
+		int n = sc.nextInt();
+		
+		List<Shape> list = new ArrayList<>();
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Shape #" + i + " data:");
+			System.out.print("Rectangle or Circle? (r/c)? ");
+			char choice = sc.next().charAt(0);
+			System.out.print("Color (BLACK/BLUE/RED): ");
+			String color = sc.next();
+			
+			if (choice == 'r') {
+				System.out.print("Width: ");
+				Double width = sc.nextDouble();
+				System.out.print("Height: ");
+				Double height = sc.nextDouble();
+				list.add(new Rectangle(Color.valueOf(color), width, height));
+			} else if (choice == 'c') {
+				System.out.print("Radius: ");
+				Double radius = sc.nextDouble();
+				list.add(new Circle(Color.valueOf(color), radius));
+			}
+		}
+		
+		System.out.println("SHAPE AREAS:");
+		for (Shape shape : list) {
+			System.out.println(String.format("%.2f", shape.area()));
+		}
+		
+		sc.close();
+	}
+
+}
